1b313b18860abd51176dfd239abb44bb40299b59
Francisco Menezes <franciscomenezesbh@gmail.com>
2021-09-27 23:26:05 -0300

    aula 01

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,14 +1,22 @@
 import React from 'react';
 import {Text, View } from 'react-native';
 
-import { Container} from './styles'
+import { Container, SearchContainer, Input, SearchButton } from './styles'
 import  Header from '../../components/Header'
+
+import { Feather } from '@expo/vector-icons' 
 
 export default function Home() {
   return (
     <Container>
       <Header title="React Prime" />
-      <Text>Tela Home</Text>
+
+      <SearchContainer>
+        <Input placeholder="Ex Vingadores" placeholderTextColor="ddd"/>
+        <SearchButton>
+            <Feather name="search" size={30} color="#fff" />
+        </SearchButton>
+      </SearchContainer>
   
     </Container>
   );
